3d6b2b0059bca641cf858d35b017d339fad51c44
The_TT_Hacker <timthacker97@gmail.com>
2017-10-21 16:36:22 +1100

    least loaded path, problem with blocked packets

--- State.java
+++ State.java
@@ -9,51 +9,51 @@ public class State<E> {
     private Node<E> node;
     private State<E> parent;
 
-    private int distFromStart;
-
+    private int    distFromStart;
+    private double percentageLoad;
 
     public State(Node<E> node, State<E> parent) {
         this.node = node;
         this.parent = parent;
 
-        this.distFromStart = Integer.MAX_VALUE;
-
+        this.distFromStart  = Integer.MAX_VALUE;
+        this.percentageLoad = 1.00;
     }
 
 
     /* === Getters === */
 
 
-    /** 
-     * gets the node of this instance of the state 
-     * 
+    /**
+     * gets the node of this instance of the state
+     *
      * @return node
      */
     public Node<E> getNode() {
         return this.node;
     }
 
-    /** 
-     * gets the parent state of this instance of state 
-     * 
+    /**
+     * gets the parent state of this instance of state
+     *
      * @return parent state
      */
     public State<E> getParent() {
         return this.parent;
     }
 
-    /** 
+    /**
      * gets the name of the state node
-     * 
-     * @return name of the state node 
+     *
+     * @return name of the state node
      */
     public E getName() {
         return this.getNode().getName();
     }
 
-    /** 
+    /**
      * gets the adjancey list of the state node
-     * 
+     *
      * @return list of edges connected to state node
      */
     public List<Edge<E>> getConnections() {
@@ -64,18 +64,26 @@ public class State<E> {
         return this.distFromStart;
     }
 
+    public double getPercentageLoad() {
+        return this.percentageLoad;
+    }
+
     /* === Setters === */
 
     public void setDistFromStart(int distFromStart) {
         this.distFromStart = distFromStart;
     }
 
+    public void setPercentageLoad(double percentageLoad) {
+        this.percentageLoad = percentageLoad;
+    }
+
     /* === Methods === */
 
-    /** 
+    /**
      * equals method - equal if names are equals
-     * 
-     * @param obj - to be compared 
+     *
+     * @param obj - to be compared
      * @return true or false depending on whether the states names are equal
      */
     @Override
@@ -86,8 +94,8 @@ public class State<E> {
         if(obj == null) return false;
         // type check and cast
         if(this.getClass() != obj.getClass()) return false;
-        State<?> o = (State<?>) obj; 
-        
+        State<?> o = (State<?>) obj;
+
         // field comparison
         if ( Objects.equals(this.getName(),o.getName()) ) return true;
         return false;
